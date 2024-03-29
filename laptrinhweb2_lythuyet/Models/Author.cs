12624f6ee89ee8a25b8d37fef71a500060e83284
TinhPham2k4 <tinhpham03042k4@gmail.com>
2024-03-29 23:48:19 +0700

    init project

---
+++ laptrinhweb2_lythuyet/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace laptrinhweb2_lythuyet.Models
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        // One-to-many relationship with books
+        public List<Book>? Books { get; set; }
+    }
+}
